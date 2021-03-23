a91cde13bd4aefbb7229b746a229bb091d08bf27
Alina1435 <81226408+Alina1435@users.noreply.github.com>
2021-03-23 15:03:05 +0500

    Add files via upload

---
+++ norm_cube.py
@@ -0,0 +1,9 @@
+from graph import*
+penColor("red")
+brushColor("blue")
+rectangle(30,30,50,50)
+brushColor("green")
+polygon([(30,30),(40,20),(60,20),(50,30),(30,30)])
+brushColor("red")
+polygon([(60,20),(60,40),(50,50),(50,30),(60,20)])
+run()
