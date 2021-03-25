1bcf70102f811be2277b767deb8ca9141bea6eac
Alina1435 <81226408+Alina1435@users.noreply.github.com>
2021-03-25 15:17:53 +0500

    Add files via upload

---
+++ triangles2.py
@@ -0,0 +1,12 @@
+from graph import*
+def triangle(x,y):
+    polygon([(x,y),(x+40,y),(x+20,y+30),(x,y)])
+brushColor('blue')
+triangle(10,10)
+brushColor('red')
+triangle(50,10)
+brushColor('green')
+triangle(30,40)
+run()
+    
+    
